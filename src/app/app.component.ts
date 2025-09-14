846850901b0e32d8be1ec4e6b1d7ec3471168124
Albert Varaksin <albeva@me.com>
2025-09-14 18:39:54 +0900

    Enable dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'kana-practice';
+
+    private mode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    constructor() {
+        this.updateTheme();
+        this.mode.addEventListener('change', () => {
+            this.updateTheme();
+        });
+    }
+
+    private updateTheme() {
+        this.setTheme('auto');
+    }
+
+    private setTheme(theme: string) {
+        if (theme === 'auto') {
+            document.documentElement.setAttribute(
+                'data-bs-theme',
+                this.systemTheme()
+            );
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme);
+        }
+    }
+
+    private systemTheme() {
+        return this.mode.matches ? 'dark' : 'light';
+    }
 }
