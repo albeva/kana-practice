768785abb98de2d070161b0bcce6c321f017a67d
Albert Varaksin <albeva@me.com>
2025-09-14 14:27:51 +0900

    Add analytics.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: true,
     firebase: {
-        projectId: 'kana-practice-eb172',
-        appId: '1:942310866666:web:cb8ee8377b49db6d182193',
-        storageBucket: 'kana-practice-eb172.firebasestorage.app',
         apiKey: 'AIzaSyAZN1cwPdBDYmN1yX6jSUvGg879eM4Nsts',
         authDomain: 'kana-practice-eb172.firebaseapp.com',
+        projectId: 'kana-practice-eb172',
+        storageBucket: 'kana-practice-eb172.firebasestorage.app',
         messagingSenderId: '942310866666',
+        appId: '1:942310866666:web:cb8ee8377b49db6d182193',
         measurementId: 'G-P1VHFDDQDT',
-    }
+    },
 };
